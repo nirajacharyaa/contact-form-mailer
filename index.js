ffbd31aa605fd5c32c836a0c2d789ca536727bbc
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-10-21 19:46:34 +0545

    resolves cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
