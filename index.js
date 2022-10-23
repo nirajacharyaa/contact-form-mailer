7516c3d437ffcaa2fa29b18df4a17ff222b9d511
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-10-23 12:00:47 +0545

    Fixed common JS error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const app = express();
-const { rateLimiter } = require("./requestLimiter");
+const rateLimiter = require("./requestLimiter");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
