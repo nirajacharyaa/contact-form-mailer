cd2b025f46538807c29c19993d837b69fb22e6d4
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-10-23 11:55:12 +0545

    Added request limit.

---
+++ requestLimiter.js
@@ -0,0 +1,8 @@
+const requestLimiter = require("express-rate-limit");
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Maximum send request reached ! Try after 15 min.",
+});
